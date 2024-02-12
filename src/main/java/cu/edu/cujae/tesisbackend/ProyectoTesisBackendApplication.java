63bc3c83fb8cee3a52b14c136bb731f4c39d9b87
Rayner Alejandro Soto Martínez <raynersoto01@gmail.com>
2024-02-11 21:26:17 -0500

    Proyecto vacío

---
+++ src/main/java/cu/edu/cujae/tesisbackend/ProyectoTesisBackendApplication.java
@@ -0,0 +1,13 @@
+package cu.edu.cujae.tesisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTesisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoTesisBackendApplication.class, args);
+    }
+
+}
